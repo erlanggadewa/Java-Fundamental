ca9883bb25361f1cf5061b7db802c802d635d701
Erlangga Dewa Sakti <erlanggadewa@student.telkomuniversity.ac.id>
2020-10-21 16:47:05 +0700

    update

--- 0000 - LATIHAN/0011 - Percobaan/wkwk.java
+++ 0000 - LATIHAN/0011 - Percobaan/wkwk.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class wkwk {
 	public static void main(String[] args) {
-		Scanner inputNafidz = new Scanner(System.in);
-		int angka = inputNafidz.nextInt();
+		Scanner userInput = new Scanner(System.in);
+		int angka = userInput.nextInt();
 		System.out.println("angka yang anda masukan adalah " + angka);
-
+		userInput.close();
 	}
 }
